2c266e94c355c84f9d3cb0e4c48293f16972ff02
johannreboulleau <johann.reboulleau@gmail.com>
2016-03-22 08:06:06 +1100

    Creation pattern titre document Absences

--- src/main/java/nc/noumea/mairie/alfresco/cmis/CmisUtils.java
+++ src/main/java/nc/noumea/mairie/alfresco/cmis/CmisUtils.java
@@ -1,7 +1,14 @@
 package nc.noumea.mairie.alfresco.cmis;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public final class CmisUtils {
 
+	private static final String UNDERSCORE = "_";
+	private static final Integer NOMBRE_CARACTERES_MAX = 25;
+	
+	private static SimpleDateFormat sdfyyyyMMdd = new SimpleDateFormat("yyyyMMdd");
 
     protected CmisUtils () { 
     }
@@ -32,6 +39,30 @@ public final class CmisUtils {
 		}
 		result += "content";
 		
+		return result;
+	}
+	
+	public static String getPatternAbsence(String typeDemande, String nom, String prenom, Date date, Integer sequence) { 
+
+		String nomCustom = "";
+		String prenomCustom = "";
+		
+		if(nom.length() + prenom.length() > NOMBRE_CARACTERES_MAX) {
+			if(nom.length() > NOMBRE_CARACTERES_MAX) {
+				nomCustom = nom.substring(0, NOMBRE_CARACTERES_MAX);
+			}else{
+				nomCustom = nom;
+				prenomCustom = prenom.substring(0, NOMBRE_CARACTERES_MAX-nom.length());
+			}
+		}else{
+			nomCustom = nom;
+			prenomCustom = prenom;
+		}
+		
+		String result = "ABS" + UNDERSCORE + typeDemande.toUpperCase() + UNDERSCORE + nomCustom.trim().toUpperCase() 
+				+ (prenomCustom.trim().equals("") ? "" : UNDERSCORE + prenomCustom.trim().toUpperCase()) + UNDERSCORE 
+				+ sdfyyyyMMdd.format(date) + UNDERSCORE + (null != sequence ? sequence : "");
+		result = result.replace(" ", UNDERSCORE);
 		return result;
 	}
 }
